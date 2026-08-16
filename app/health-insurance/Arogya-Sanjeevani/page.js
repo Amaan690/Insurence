d5571bdb45f6db677ca23e622840e5b75c326c9d
Amaan690 <mdamanullah690@gmail.com>
2026-08-16 22:36:54 +0530

    Add health insurance done

---
+++ app/health-insurance/Arogya-Sanjeevani/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function ArogyaSanjeevani() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">Arogya Sanjeevani</h1>
+      <p className="text-lg text-center max-w-md">
+       Arogya Sanjeevani is a comprehensive health insurance policy designed to provide financial protection against a wide range of medical expenses. It offers coverage for hospitalization, surgeries, day-care procedures, and other eligible healthcare costs. The policy helps reduce the financial burden of unexpected medical emergencies and ensures that you can access quality healthcare when you need it most. With its simple and affordable coverage, Arogya Sanjeevani provides peace of mind and helps you stay financially prepared for unforeseen health-related expenses.
+      </p>
+    </div>   
+  );
+}
---
+++ app/health-insurance/Super-Top-Up/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function SuperTopUp() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-4">1 Cr Super Top-up</h1>
+      <p className="text-lg text-center max-w-md">
+        The 1 Cr Super Top-up plan is designed to provide additional financial protection over and above your existing health insurance coverage. With a high sum insured of ₹1 crore, this plan helps safeguard you and your family against the rising costs of major medical treatments and unexpected healthcare expenses. It provides enhanced coverage for hospitalization, surgeries, critical illnesses, and other eligible medical expenses once the applicable deductible is crossed. The plan is an ideal choice for individuals who already have a basic health insurance policy but want an extra layer of protection against high-value medical expenses. By offering substantial additional coverage at an affordable cost, the 1 Cr Super Top-up plan provides greater financial security, reduces the risk of out-of-pocket expenses, and gives you peace of mind during medical emergencies.
+      </p>
+    </div>   
+  );
+}
